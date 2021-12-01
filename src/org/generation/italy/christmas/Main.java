404de3f8f958c608d5293bc7d63b9ad705cab152
JEROMEBRANCHETTI <jeromebranchetti00@gmail.com>
2021-12-01 19:12:40 +0100

    aggiunta Exception error e correzione ciclo

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -6,32 +6,56 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		
 		ArrayList<String> listaDesideri = new ArrayList<String>();
 		Scanner scan = new Scanner(System.in);
 		String elemento;
-		String risposta;
-		boolean check = true;
+		String risposta = "";
+		boolean continua = true;
+		//boolean vaBene = false;
 		do {
 			System.out.print("Aggiungi elemento alla lista dei desideri: ");
 			elemento = scan.nextLine();
 			listaDesideri.add(elemento);
-			System.out.print("La tua lista contiene " + listaDesideri.size());
-			System.out.println(" desideri.");
-			System.out.print("Continuare? (s/n) ");
-			risposta = scan.nextLine();
-			if (risposta.equals("n")) {
-				check = false;
-			}
-			else if (risposta.equals("s")) {
-				check = true;
+			if (listaDesideri.size() == 1) {
+				System.out.print("La tua lista contiene " + listaDesideri.size());
+				System.out.println(" desiderio.");
 			}
 			else {
-				System.out.println("Errore");
-				break;
+				System.out.print("La tua lista contiene " + listaDesideri.size());
+				System.out.println(" desideri.");
 			}
-		} while (check);
+			System.out.print("Continuare? (s/n) ");
+			boolean vaBene = false;
+			while(!vaBene) {
+				try {
+					risposta = scan.nextLine();
+					if (risposta.equals("n")) {
+						continua = false;
+						vaBene = true;
+					}
+					else if (risposta.equals("s")) {
+						continua = true;
+						vaBene = true;
+					}
+					
+					else{
+						throw new Exception("Errore... non hai inserito i caratteri tra le opzioni");
+					}
+					
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					System.out.println(e.getMessage());
+					System.out.println("Riprova (s/n):" + "\t");
+				}
+			}
+			
+			
+			
+			
+		} while (continua);
 		//ordianiamo la lista in ordine alfabetico
 		Collections.sort(listaDesideri);
 		System.out.println("Ecco la tua lista:");
